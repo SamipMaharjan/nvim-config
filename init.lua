6a515fef59cd6db060bd60bf849f9b4e45734696
SamipMaharjan <adsrb15@gmail.com>
2025-11-20 14:17:15 +0545

    fix:: surround unsurround with mini.surround fix

--- init.lua
+++ init.lua
@@ -1,9 +1,14 @@
 vim.g.mapleader = " "
 require("config.lazy")
 
+-- Vim options
 vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.splitright = true
+vim.opt.clipboard = "unnamedplus"
 
 
 -- QuickFix Keymaps
@@ -12,19 +17,19 @@ vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 vim.keymap.set("n", "<M-c>", "<cmd>cclose<CR>")
 
 vim.diagnostic.config({
-  virtual_text = true, -- Show error text inline
-  signs = true,        -- Show error signs in gutter
-  underline = true,    -- Show underlines
-  update_in_insert = false,
-  severity_sort = true,
-  float = {
-    focusable = false,
-    style = "minimal",
-    border = "rounded",
-    source = true,
-    header = "",
-    prefix = "",
-  },
+    virtual_text = true, -- Show error text inline
+    signs = true,      -- Show error signs in gutter
+    underline = true,  -- Show underlines
+    update_in_insert = false,
+    severity_sort = true,
+    float = {
+        focusable = false,
+        style = "minimal",
+        border = "rounded",
+        source = true,
+        header = "",
+        prefix = "",
+    },
 })
 
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
@@ -34,21 +39,21 @@ vim.keymap.set({ "n", "v" }, "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
 
 vim.api.nvim_create_autocmd('TermOpen', {
-  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-  end,
+    group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+    callback = function()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end,
 })
 
 vim.keymap.set("n", "<space>st", function()
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 15)
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("J")
+    vim.api.nvim_win_set_height(0, 15)
 end)
 
 -- FOR ALT 1-0 TAB SWITCHING
 for i = 1, 9 do
-  vim.keymap.set('n', '<M-' .. i .. '>', '<cmd>tabn ' .. i .. '<CR>')
+    vim.keymap.set('n', '<M-' .. i .. '>', '<cmd>tabn ' .. i .. '<CR>')
 end
--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -7,11 +7,17 @@ return {
     config = function()
       require('mini.statusline').setup()
       require('mini.surround').setup({
-        add = "gsa",      -- Add surrounding
-        delete = "gsd",   -- Delete surrounding
-        replace = "gsr",  -- Replace surrounding
-        find = "gsf",     -- Find surrounding
-        highlight = "gsh" -- Highlight surrounding
+        mappings = {
+          add = "sa",            -- Add surrounding
+          delete = "sd",         -- Delete surrounding
+          replace = "sr",        -- Replace surrounding
+          find = "sf",           -- Find surrounding (to the right)
+          find_left = "sF",      -- Find surrounding (to the left)
+          highlight = "sh",      -- Highlight surrounding
+          update_n_lines = "sn", -- Update search n_lines
+          suffix_last = "l",     -- Suffix to search with "last"
+          suffix_next = "n",     -- Suffix to search with "next"
+        },
       })
     end,
   },
